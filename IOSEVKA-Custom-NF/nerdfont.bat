67f251e2cfde2fd205c896d28993fd5a6916865c
Saumyajyoti M <saumyajyoti@gmail.com>
2023-07-27 00:54:43 +0530

    customized regular miosevka v4

--- IOSEVKA-Custom-NF/nerdfont.bat
+++ IOSEVKA-Custom-NF/nerdfont.bat
@@ -6,17 +6,22 @@ setlocal
 :: - https://github.com/be5invis/Iosevka/blob/main/doc/custom-build.md#building
 :: - https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/FontPatcher.zip
 
-:: set IOSEVKA_PATH
+set IOSEVKA_PATH="D:\SOURCE\github\Iosevka"
 cd /d %IOSEVKA_PATH%
+rmdir /S /Q "%IOSEVKA_PATH%\dist"
 :: Copy private-build-plans.toml here
-call npm run build -- ttf::Miosevka
+call npm run build -- ttf::miosevka
 
 SET "PATH=C:\Program Files (x86)\FontForgeBuilds\bin>;%PATH%"
-:: SET OUTPATH
+SET FFPYTHON_EXE="C:\Program Files (x86)\FontForgeBuilds\bin\ffpython.exe"
+SET OUTPATH="D:\Font\Miosevka4"
 :: SET NERDFONT_PATCHER_PATH [ download from https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/FontPatcher.zip]
 cd /d %OUTPATH%
-SET FFPYTHON_EXE="C:\Program Files (x86)\FontForgeBuilds\bin\ffpython.exe"
-%FFPYTHON_EXE% "D:\SETUP.tmp\nerdfont\font-patcher" -c -l --careful "D:\source\github\iosevka\dist\Miosevka\ttf\Miosevka-bold.ttf"
-%FFPYTHON_EXE% "D:\SETUP.tmp\nerdfont\font-patcher" -c -l --careful "D:\source\github\iosevka\dist\Miosevka\ttf\Miosevka-boldItalic.ttf"
-%FFPYTHON_EXE% "D:\SETUP.tmp\nerdfont\font-patcher" -c -l --careful "D:\source\github\iosevka\dist\Miosevka\ttf\Miosevka-italic.ttf"
-%FFPYTHON_EXE% "D:\SETUP.tmp\nerdfont\font-patcher" -c -l --careful "D:\source\github\iosevka\dist\Miosevka\ttf\Miosevka-regular.ttf"
+SET NERDFONT_PATCHER_PATH="D:\SETUP.tmp\nerdfont\font-patcher"
+
+%FFPYTHON_EXE% %NERDFONT_PATCHER_PATH% -c -l "%IOSEVKA_PATH%\dist\miosevka\ttf\miosevka-regular.ttf"
+%FFPYTHON_EXE% %NERDFONT_PATCHER_PATH% -c -l "%IOSEVKA_PATH%\dist\miosevka\ttf\miosevka-italic.ttf"
+%FFPYTHON_EXE% %NERDFONT_PATCHER_PATH% -c -l "%IOSEVKA_PATH%\dist\miosevka\ttf\miosevka-bold.ttf"
+%FFPYTHON_EXE% %NERDFONT_PATCHER_PATH% -c -l "%IOSEVKA_PATH%\dist\miosevka\ttf\miosevka-boldItalic.ttf"
+%FFPYTHON_EXE% %NERDFONT_PATCHER_PATH% -c -l "%IOSEVKA_PATH%\dist\miosevka\ttf\miosevka-semibold.ttf"
+%FFPYTHON_EXE% %NERDFONT_PATCHER_PATH% -c -l "%IOSEVKA_PATH%\dist\miosevka\ttf\miosevka-semiboldItalic.ttf"
