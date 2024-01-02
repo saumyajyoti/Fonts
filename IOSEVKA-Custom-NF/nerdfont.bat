cfce4dc540ad5162c8410d68caa7b252c9b9c860
Saumyajyoti M <saumyajyoti@gmail.com>
2024-01-02 13:08:11 +0530

    Add RIOSEVKA font
    Updated Font versions to 7.

--- IOSEVKA-Custom-NF/nerdfont.bat
+++ IOSEVKA-Custom-NF/nerdfont.bat
@@ -1,38 +1,57 @@
+@echo off
 setlocal
 :: TODO/prerequisites in comments
 :: tested in Windows11 setup
 
 :: install nodejs, ttfautohint, fontforge python 3 as mentioned in 
 :: - https://github.com/be5invis/Iosevka/blob/main/doc/custom-build.md#building
-:: - https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/FontPatcher.zip
+:: - https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/FontPatcher.zip
 
-set IOSEVKA_PATH="D:\SOURCE\github\Iosevka"
-cd /d %IOSEVKA_PATH%
-rmdir /S /Q "%IOSEVKA_PATH%\dist"
-git pull --depth=1
-copy /Y %~dp0\private-build-plans.toml %IOSEVKA_PATH%
-
-call npm install
-call npm run build -- ttf::miosevka
-
+SET IOSEVKA_PATH="D:\SOURCE\github\Iosevka"
 SET "PATH=C:\Program Files (x86)\FontForgeBuilds\bin>;%PATH%"
 SET FFPYTHON_EXE="C:\Program Files (x86)\FontForgeBuilds\bin\ffpython.exe"
-SET FONTVER=Miosevka6
-SET OUTPATH="D:\Font\%FONTVER%"
-:: SET NERDFONT_PATCHER_PATH [ download from https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/FontPatcher.zip]
+SET FONTVERNUM=7
+SET OUTPATH="D:\Font\Miosevka%FONTVERNUM%"
+:: SET NERDFONT_PATCHER_PATH [ download from path above]
+SET NERDFONT_PATCHER_PATH="D:\SETUP.tmp\nerdfont\font-patcher"
+SET FONTVER=Miosevka%FONTVERNUM%
+echo =======================================================
+echo Sync Iosevka
+cd /d %IOSEVKA_PATH%
+rmdir /S /Q "%IOSEVKA_PATH%\dist"
+
+git pull --depth=1
+
+call npm install
+echo =======================================================
+echo Build Miosevka
+copy /Y %~dp0\miosevka-build-plans.toml  %IOSEVKA_PATH%\private-build-plans.toml
+call npm run build -- ttf::Miosevka
+echo =======================================================
+echo Build Riosevka
+copy /Y %~dp0\riosevka-build-plans.toml  %IOSEVKA_PATH%\private-build-plans.toml
+call npm run build -- ttf::Riosevka
+
 mkdir %OUTPATH%
 cd /d %OUTPATH%
-SET NERDFONT_PATCHER_PATH="D:\SETUP.tmp\nerdfont\font-patcher"
-
+echo =======================================================
+echo patch fonts
 %FFPYTHON_EXE% %NERDFONT_PATCHER_PATH% -c "%IOSEVKA_PATH%\dist\miosevka\ttf\miosevka-regular.ttf"
 %FFPYTHON_EXE% %NERDFONT_PATCHER_PATH% -c "%IOSEVKA_PATH%\dist\miosevka\ttf\miosevka-italic.ttf"
 %FFPYTHON_EXE% %NERDFONT_PATCHER_PATH% -c "%IOSEVKA_PATH%\dist\miosevka\ttf\miosevka-bold.ttf"
 %FFPYTHON_EXE% %NERDFONT_PATCHER_PATH% -c "%IOSEVKA_PATH%\dist\miosevka\ttf\miosevka-boldItalic.ttf"
 
-copy /Y %~dp0\license.* %OUTPATH%
+%FFPYTHON_EXE% %NERDFONT_PATCHER_PATH% -c "%IOSEVKA_PATH%\dist\riosevka\ttf\riosevka-regular.ttf"
+%FFPYTHON_EXE% %NERDFONT_PATCHER_PATH% -c "%IOSEVKA_PATH%\dist\riosevka\ttf\riosevka-italic.ttf"
+%FFPYTHON_EXE% %NERDFONT_PATCHER_PATH% -c "%IOSEVKA_PATH%\dist\riosevka\ttf\riosevka-bold.ttf"
+%FFPYTHON_EXE% %NERDFONT_PATCHER_PATH% -c "%IOSEVKA_PATH%\dist\riosevka\ttf\riosevka-boldItalic.ttf"
+echo =======================================================
+echo Copy Files
+copy /Y %~dp0\..\license.* %OUTPATH%
 copy /Y "%IOSEVKA_PATH%\dist\miosevka\ttf\*.ttf" %OUTPATH%
+copy /Y "%IOSEVKA_PATH%\dist\riosevka\ttf\*.ttf" %OUTPATH%
 cd ..
- 
+echo create %FONTVER%.zip 
 tar.exe -a -c -f "%FONTVER%.zip" %OUTPATH%
 
 explorer .
